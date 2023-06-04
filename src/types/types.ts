1bacbb8c6f80d83c459ce7cc29a48cfde381cd6f
Mirbek <emirbekov.mirbek@gmail.com>
2023-06-04 20:13:01 +0600

    fix

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,60 +1,63 @@
 export interface ILocation {
-  lat: number;
-  lon: number;
-  country: string,
-  city: string,
+  lat: number
+  lon: number
+  country: string
+  city: string
 }
 
 type weather = {
-  id: number,
-  main: string,
-  description: string,
+  id: number
+  main: string
+  description: string
   icon: string
 }
+
 type temp = {
-  day: number,
+  day: number
   eve: number
 }
 
-interface currentType {
-  dt: number;
-  temp: number;
-  sunrise: number;
-  weather: weather[];
-  'wind_speed': number,
-  clouds: number;
-  humidity: number;
+interface ICurrentType {
+  dt: number
+  temp: number
+  sunrise: number
+  weather: weather[]
+  'wind_speed': number
+  clouds: number
+  humidity: number
 }
 export interface IDaily {
-  dt: number,
-  temp: temp,
-  sunrise: number,
-  weather: weather[],
-  'wind_speed': number,
+  dt: number
+  temp: temp
+  sunrise: number
+  weather: weather[]
+  'wind_speed': number
   clouds: number
   humidity: number
 }
 
 export interface IWeather {
-  lat: number;
-  lon: number;
-  timezone: number;
-  current: currentType;
+  lat: number
+  lon: number
+  timezone: number
+  current: ICurrentType
   daily: IDaily[]
 
 }
 
 interface LocalNames {
-  [key: string]: string;
+  [key: string]: string
 }
+
 type coord = {
   lat: number
   lon: number
 }
+
 export interface ICity {
-  coord: coord;
-  state: string;
-  name: string;
-  sys: LocalNames;
+  coord: coord
+  state: string
+  name: string
+  sys: LocalNames
   id: number
 }
